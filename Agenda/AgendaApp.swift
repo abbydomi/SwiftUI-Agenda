8cbe25a868f1ac462d705051c23cdf6e588b7902
Abby Dominguez <abbytorade@icloud.com>
2023-01-09 20:14:52 +0100

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Abby Dominguez on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
